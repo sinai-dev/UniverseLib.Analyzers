cf84f4a9f7070972152b233255ac1db103c14c86
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-01-30 18:28:24 +1100

    Prevent Analyzers running in UniverseLib.Runtime.Il2Cpp

--- UniverseLib.Analyzers/Unity/LayoutGroupChildControlAnalyzer.cs
+++ UniverseLib.Analyzers/Unity/LayoutGroupChildControlAnalyzer.cs
@@ -42,7 +42,7 @@ namespace UniverseLib.Analyzers.Unity
 
             if (context.ContainingSymbol?.ContainingType is ITypeSymbol contextType)
             {
-                if (contextType.NamespaceEquals("UniverseLib.Runtime.Il2Cpp") && contextType.Name == "Il2CppProvider")
+                if (contextType.NamespaceEquals("UniverseLib.Runtime.Il2Cpp"))
                     return;
             }
 
